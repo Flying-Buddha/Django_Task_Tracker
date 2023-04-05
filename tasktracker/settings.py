49d9349c19bf6bdf8b0b7bf1e002411e59ec0b34
Flying-Buddha <Flying-Buddha@users.noreply.github.com>
2023-04-06 00:15:18 +0200

    Views created

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Created Apps
+    'account.apps.AccountConfig',
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
